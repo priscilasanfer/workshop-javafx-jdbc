7f5f7c4fccd388a95868bac41c93d4a0ab87df93
Fonseca, Priscila Santos Ferreira <priscila.fonseca@wirecard.com>
2020-05-26 22:30:14 -0300

    Update department and some fixes in the code

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,5 +1,12 @@
 package gui;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
 import db.DbException;
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
@@ -8,16 +15,13 @@ import gui.util.Utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
 import model.exceptions.ValidationException;
 import model.services.DepartmentService;
-
-import java.net.URL;
-import java.util.*;
 
 public class DepartmentFormController implements Initializable {
 
@@ -37,10 +41,10 @@ public class DepartmentFormController implements Initializable {
     private Label labelErrorName;
 
     @FXML
-    private Button btCancel;
+    private Button btSave;
 
     @FXML
-    Button btSave;
+    private Button btCancel;
 
     public void setDepartment(Department entity) {
         this.entity = entity;
@@ -67,10 +71,12 @@ public class DepartmentFormController implements Initializable {
             service.saveOrUpdate(entity);
             notifyDataChangeListeners();
             Utils.currentStage(event).close();
-        } catch (ValidationException e) {
+        }
+        catch (ValidationException e) {
             setErrorMessages(e.getErrors());
-        } catch (DbException e) {
-            Alerts.showAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
+        }
+        catch (DbException e) {
+            Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
         }
     }
 
@@ -79,7 +85,6 @@ public class DepartmentFormController implements Initializable {
             listener.onDataChanged();
         }
     }
-
 
     private Department getFormData() {
         Department obj = new Department();
@@ -91,7 +96,6 @@ public class DepartmentFormController implements Initializable {
         if (txtName.getText() == null || txtName.getText().trim().equals("")) {
             exception.addError("name", "Field can't be empty");
         }
-
         obj.setName(txtName.getText());
 
         if (exception.getErrors().size() > 0) {
--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,15 @@
 package gui;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
 import application.Main;
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -11,8 +17,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -21,11 +28,6 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.List;
-import java.util.ResourceBundle;
 
 public class DepartmentListController implements Initializable, DataChangeListener {
 
@@ -39,6 +41,9 @@ public class DepartmentListController implements Initializable, DataChangeListen
 
     @FXML
     private TableColumn<Department, String> tableColumnName;
+
+    @FXML
+    private TableColumn<Department, Department> tableColumnEDIT;
 
     @FXML
     private Button btNew;
@@ -59,13 +64,13 @@ public class DepartmentListController implements Initializable, DataChangeListen
     }
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize(URL url, ResourceBundle rb) {
         initializeNodes();
     }
 
     private void initializeNodes() {
         tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
-        tableColumnId.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
 
         // Macete para a tableView acompanhar o tamanho da janela
         Stage stage = (Stage) Main.getMainScene().getWindow();
@@ -79,9 +84,10 @@ public class DepartmentListController implements Initializable, DataChangeListen
         List<Department> list = service.findAll();
         obsList = FXCollections.observableArrayList(list);
         tableViewDepartment.setItems(obsList);
+        initEditButtons();
     }
 
-    public void createDialogForm(Department obj, String absoluteName, Stage parentStage) {
+    private void createDialogForm(Department obj, String absoluteName, Stage parentStage) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
             Pane pane = loader.load();
@@ -92,21 +98,39 @@ public class DepartmentListController implements Initializable, DataChangeListen
             controller.subscribeDataChangeListener(this);
             controller.updateFormData();
 
-            Stage diaologStage = new Stage();
-            diaologStage.setTitle("Enter Departament data");
-            diaologStage.setScene(new Scene(pane));
-            diaologStage.setResizable(false);
-            diaologStage.initOwner(parentStage);
-            diaologStage.initModality(Modality.WINDOW_MODAL);
-            diaologStage.showAndWait();
-
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Enter Department data");
+            dialogStage.setScene(new Scene(pane));
+            dialogStage.setResizable(false);
+            dialogStage.initOwner(parentStage);
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.showAndWait();
         } catch (IOException e) {
-            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
         }
     }
 
     @Override
     public void onDataChanged() {
         updateTableView();
+    }
+
+    private void initEditButtons() {
+        tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+        tableColumnEDIT.setCellFactory(param -> new TableCell<Department, Department>() {
+            private final Button button = new Button("edit");
+
+            @Override
+            protected void updateItem(Department obj, boolean empty) {
+                super.updateItem(obj, empty);
+                if (obj == null) {
+                    setGraphic(null);
+                    return;
+                }
+                setGraphic(button);
+                button.setOnAction(
+                        event -> createDialogForm(obj, "/gui/DepartmentForm.fxml", Utils.currentStage(event)));
+            }
+        });
     }
 }
